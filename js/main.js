7c30db962886b960685acb82fb48d1802233180f
hamatoshiki <s20am071@seinan-gakuin.jp>
2021-06-02 22:40:37 +0900

    動的

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
-const A=(80 + 90 + 60 + 85 + 100);
-const B = (40 + 30 + 50 + 25 + 60);
-console.log(A, B);
-if (A => 300 && B >= 300) {
-  console.log("素晴らしい")
-} if (A >= 300 || B >= 300) {
-  console.log("普通");
-} if (A < 300 && B < 300) {
-  console.log("頑張ろう");
-}
-const price = 1800;
-const result = (price < 1000) ? "安い" : (price > 2000) ? "高い" : "丁度いい";
-console.log(result);
-
-const array = ["1"," 2", "3","4", "5",];
-const indexOfii = array.indexOf("1");
-console.log(indexOfii);
-console.log(array[indexOfii]);
-console.log(array.indexOfii("9"));
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  const target = document.getElementById("menu");
+  target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
+});
