3bfc90cc7a2c3fbebdbf0cbf398d1abfe288804f
hamatoshiki <s20am071@seinan-gakuin.jp>
2021-06-06 15:02:23 +0900

    問題

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 const target = document.getElementById("menu");
 target.addEventListener('click', () => {
-  const target = document.getElementById("menu");
   target.classList.toggle('open');
-  const nav = document.getElementById("nav");
-  nav.classList.toggle('in');
 });
